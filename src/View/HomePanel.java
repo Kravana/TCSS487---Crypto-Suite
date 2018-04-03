68c0d47a933498565fc7e7e12a200571884e14a9
Kevin Ravana <kevravana@gmail.com>
2018-04-03 12:25:34 -0700

    panel navigation added

--- src/View/HomePanel.java
+++ src/View/HomePanel.java
@@ -1,6 +1,11 @@
 package View;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 
 /**
  * @author Kevin Ravna
@@ -8,8 +13,49 @@ import javax.swing.*;
  */
 public class HomePanel extends JPanel {
 
-    public HomePanel() {
 
+    private final String[] SERVICE_NAMES = {"Compute hash"};
+
+    private final JPanel[] PANELS = {new ComputeHashPanel()};
+
+
+    public HomePanel() {
+        super();
+        initialize();
     }
+
+    private void initialize() {
+        this.setLayout(new FlowLayout());
+        this.add(new JLabel("Select a service:"));
+        this.add(serviceSelector(SERVICE_NAMES));
+    }
+
+    private JComboBox<String> serviceSelector(final String[] theServices) {
+        JComboBox<String> serviceSelector = new JComboBox<>(theServices);
+        serviceSelector.addActionListener(e -> {
+            JComboBox combo = (JComboBox)e.getSource();
+            String currentSelection = (String)combo.getSelectedItem();
+            changePanel(currentSelection);
+        });
+
+
+        return serviceSelector;
+    }
+
+    private void changePanel(final String thePanelName) {
+        int index = 0;
+        for (final String name : SERVICE_NAMES) {
+            if (name.equals(thePanelName)) {
+                break;
+            } else {
+                index++;
+            }
+        }
+        JFrame frame = (JFrame)SwingUtilities.getWindowAncestor(this);
+        frame.getContentPane().removeAll();
+        frame.getContentPane().add(PANELS[index]);
+        frame.revalidate();
+    }
+
 
 }
